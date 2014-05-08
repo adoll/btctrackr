3f8abaa27c1d3edc8a849f0d56a932d7d721648a
cricket4lyfe921 <vaidhymurti@gmail.com>
2014-05-08 04:24:19 -0400

    Testing cluster lookup with AJAX
    cluster lookup

---
+++ website/btctrackr_ajax.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once("db_functions.php");
+
+// This is an AJAX handler for various btctrackr core functions
+if((strcmp($_POST['function_name'], 'get_cluster_from_address')==0) && isset($_POST["address"]))
+{
+	$mysqli = db_connect();
+	$address = $mysqli->real_escape_string($_POST["address"]);
+	$query = "SELECT cluster FROM " . MAIN_TABLE_NAME . " WHERE address = $address";
+	$result = $mysqli->query($query);
+
+	if($row = $result->fetch_assoc())
+	{
+	    return_ajax_success("success", $row["cluster"]);
+	}
+
+	return_ajax_error();
+}
+// return error
+else 
+{
+	return_ajax_error("There was error connecting to the server.");
+}
+
+
+function return_ajax_success($message='Your request processed successfully.', $response_data=array()) {
+
+	echo(json_encode(array(
+		'status' => 'success',
+		'message' => $message,
+		'response_data' => $response_data
+	)));
+	exit;
+
+}
+
+function return_ajax_error($message='There was error during your request.') {
+
+	echo(json_encode(array(
+		'status' => 'error',
+		'message' => $message,
+	)));
+	exit;
+
+}
+
+?>
