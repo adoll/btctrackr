b139dc3b1be969243e3ba4b2a1b03831e3e3f33b
cricket4lyfe921 <vaidhymurti@gmail.com>
2014-05-11 13:56:51 -0400

    added missing require
    require

--- website/btctrackr_ajax.php
+++ website/btctrackr_ajax.php
@@ -1,6 +1,10 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
 require_once("db_functions.php");
+require_once("btctrackr_functions.php");
 
 define("MAIN_TABLE_NAME", "test");
 
