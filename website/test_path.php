08b493741d3fba79c75296a5cee9cfa2a3f49d06
cricket4lyfe921 <vaidhymurti@gmail.com>
2014-05-13 11:35:09 -0400

    added to test_path
    variable addresses

--- website/test_path.php
+++ website/test_path.php
@@ -4,8 +4,11 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
 
-$addr1 = "12cbQLTFMXRnSzktFkuoG3eHoMeFtpTu3S";
-$addr2 = "1DUDsfc23Dv9sPMEk5RsrtfzCw5ofi5sVW";
+//$addr1 = "12cbQLTFMXRnSzktFkuoG3eHoMeFtpTu3S";
+//$addr2 = "1DUDsfc23Dv9sPMEk5RsrtfzCw5ofi5sVW";
+
+$addr1 = $_GET["addr1"];
+$addr2 = $_GET["addr2"];
 
 chdir("/home/ubuntu/btctrackr/parser");
 exec("sudo ./path $addr1 $addr2", $output);
