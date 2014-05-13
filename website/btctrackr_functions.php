a56b1286f5d5bce0c4f4691b35773f9a282f87e9
cricket4lyfe921 <vaidhymurti@gmail.com>
2014-05-13 12:52:21 -0400

    changed table name to production in btctrackr_functions
    test -> production

--- website/btctrackr_functions.php
+++ website/btctrackr_functions.php
@@ -6,7 +6,7 @@ ini_set('display_errors', '1');
 // This file contains core btctrackr functions
 require_once("db_functions.php");
 
-define("MAIN_TABLE_NAME", "test");
+define("MAIN_TABLE_NAME", "production");
 
 $result_array = array();
 
