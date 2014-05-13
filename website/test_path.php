246655d4a529ba6aeb6205295276a44994b78e94
cricket4lyfe921 <vaidhymurti@gmail.com>
2014-05-13 02:34:36 -0400

    use chdir
    please work

--- website/test_path.php
+++ website/test_path.php
@@ -7,7 +7,7 @@ ini_set('display_errors', '1');
 $addr1 = "12cbQLTFMXRnSzktFkuoG3eHoMeFtpTu3S";
 $addr2 = "1DUDsfc23Dv9sPMEk5RsrtfzCw5ofi5sVW";
 
-shell_exec("cd /home/ubuntu/btctrackr/parser");
+chdir("/home/ubuntu/btctrackr/parser");
 exec("./path $addr1 $addr2", $output);
 
 var_dump($output);
