8630c6e7d3fc4cd49926939bedd4f338cfad979f
cricket4lyfe921 <vaidhymurti@gmail.com>
2014-05-13 01:39:20 -0400

    test_path.php
    test the path.cpp call

---
+++ website/test_path.php
@@ -0,0 +1,15 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+
+$addr1 = "12cbQLTFMXRnSzktFkuoG3eHoMeFtpTu3S";
+$addr2 = "1DUDsfc23Dv9sPMEk5RsrtfzCw5ofi5sVW";
+
+exec("/home/ubuntu/btctrackr/parser/path $addr1 $addr2", $output);
+
+var_dump($output);
+
+
+?>
